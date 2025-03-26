24e75bd37767c2af86e45bfe65f187342ee676d8
Mequanint <mequanint.acham@hst-et.com>
2025-03-26 09:20:06 +0300

    hr remote application

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,27 @@
-import type { NextConfig } from "next";
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+import { FederatedTypesPlugin } from "@module-federation/typescript";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const federatedConfig:any = {
+  name: "hr-remote",
+  filename: "static/chunks/remoteEntry.js",
+  exposes: {
+    "./Home": "./src/components/Home.tsx",
+  },
+  shared: {},
+};
+
+const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  webpack(config:any, options:any) {
+    config.plugins.push(
+      new NextFederationPlugin(federatedConfig),
+      new FederatedTypesPlugin({ federationConfig: federatedConfig })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        home  form HR componnts
+
+    </div>
+  )
+}
+
+export default Home
